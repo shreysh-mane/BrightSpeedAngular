3a66b62b1ed744dde91a43b2a969da10d2843ba5
Shreysh Mane <shreysh.mane12@gmail.com>
2024-03-01 13:06:26 +0530

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Servicedemo1Component } from './servicedemo1/servicedemo1.component';
+import { Servicedemo2Component } from './servicedemo2/servicedemo2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'service1',component:Servicedemo1Component},
+  {path:'service2',component:Servicedemo2Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
